9078e952376613cd174c48eb654c05dbb985cef2
Kristine Du <kristine.a.du@gmail.com>
2020-02-18 07:32:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -145,7 +145,7 @@ def runner
   card_total = hit?(sum)
   until card_total > 21
     display_card_total(card_total)
-    card_total += hit?(card_total) 
+    card_total = hit?(card_total) 
   end
   end_game(card_total)
 end
