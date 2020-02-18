d07980a9016f382bb518203af35f6b14a266aabe
Kristine Du <kristine.a.du@gmail.com>
2020-02-18 06:57:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -118,7 +118,14 @@ end
 
 def hit?(card_total)
   prompt_user
-  get_user_input
+  input = get_user_input
+  if input == 'h'
+  
+  elsif input == 's'
+  
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
