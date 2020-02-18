49b837c289efc3865c3a9d010ec830c46ba10c3c
Kristine Du <kristine.a.du@gmail.com>
2020-02-18 07:25:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -143,7 +143,7 @@ def runner
   welcome
   sum = initial_round
   card_total = hit?(sum)
-  while card_total <= 21 do
+  hit?(card_total) until card_total <= 21 do
     display_card_total
     card_total = hit?(card_total)
   end
